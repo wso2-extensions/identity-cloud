f9de2c56cd7834b4428925f8e81dfe32c95b3487
Rushmin Fernando <rushmin@wso2.com>
2016-12-07 15:30:58 +0530

    IDNCLOUD-80 : Adding a session filter to handle sessions.

---
+++ components/org.wso2.carbon.identity.cloud.web.jaggery/src/main/java/org/wso2/carbon/identity/cloud/web/jaggery/SessionFilter.java
@@ -0,0 +1,52 @@
+package org.wso2.carbon.identity.cloud.web.jaggery;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Servlet filter which does session validation.
+ */
+public class SessionFilter implements Filter{
+
+    public static final String LOGIN_PAGE_URL = "/login.jag";
+    public static final String SESSION_ATTRIBUTE_NAME_USER = "user";
+
+    private List<String> whiteListedUrls;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        // White list login related resource URLs.
+        whiteListedUrls = new ArrayList<String>();
+        whiteListedUrls.add(LOGIN_PAGE_URL);
+        whiteListedUrls.add("/samlsso.jag");
+        whiteListedUrls.add("/acs");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession();
+        String requestUrl = request.getServletPath();
+
+        if(whiteListedUrls.contains(requestUrl) || (session != null && session.getAttribute(SESSION_ATTRIBUTE_NAME_USER) != null)){
+            filterChain.doFilter(servletRequest, servletResponse);
+        }else{
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.sendRedirect(request.getContextPath() + LOGIN_PAGE_URL);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
