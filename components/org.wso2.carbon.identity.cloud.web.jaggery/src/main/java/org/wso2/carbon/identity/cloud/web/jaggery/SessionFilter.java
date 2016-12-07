b6d56c473d798c61168e3eb2f492856ccb17b88a
malithie <malithi.eamm@gmail.com>
2016-12-07 22:40:48 +0530

    IDNCLOUD-80: Removed unwanted dependency and did code formatting.

--- components/org.wso2.carbon.identity.cloud.web.jaggery/src/main/java/org/wso2/carbon/identity/cloud/web/jaggery/SessionFilter.java
+++ components/org.wso2.carbon.identity.cloud.web.jaggery/src/main/java/org/wso2/carbon/identity/cloud/web/jaggery/SessionFilter.java
@@ -1,17 +1,22 @@
 package org.wso2.carbon.identity.cloud.web.jaggery;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * The Servlet filter which does session validation.
  */
-public class SessionFilter implements Filter{
+public class SessionFilter implements Filter {
 
     public static final String LOGIN_PAGE_URL = "/login.jag";
     public static final String SESSION_ATTRIBUTE_NAME_USER = "user";
@@ -30,15 +35,17 @@ public class SessionFilter implements Filter{
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession();
         String requestUrl = request.getServletPath();
 
-        if(whiteListedUrls.contains(requestUrl) || (session != null && session.getAttribute(SESSION_ATTRIBUTE_NAME_USER) != null)){
+        if (whiteListedUrls.contains(requestUrl) || (session != null && session.getAttribute
+                (SESSION_ATTRIBUTE_NAME_USER) != null)) {
             filterChain.doFilter(servletRequest, servletResponse);
-        }else{
+        } else {
             HttpServletResponse response = (HttpServletResponse) servletResponse;
             response.sendRedirect(request.getContextPath() + LOGIN_PAGE_URL);
         }
